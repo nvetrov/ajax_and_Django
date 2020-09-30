2b4908c941ccd3f907db39e5de98eb87f91e0cda
nvetrov@gmail.com <C1vmdpalc33>
2020-09-30 15:46:50 +0300

    CBV

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -16,14 +16,15 @@ Including another URLconf
 
 from django.urls import path
 from my_app.views import (
-    indexView,
-    postFriend, checkNickName,
-)
+    # indexView,
+    # postFriend, checkNickName, FriendView,
+    FriendView)
 
 urlpatterns = [
     # ... other urls
-    path('', indexView),
-    path('get/ajax/validate/nickname', checkNickName, name="validate_nickname"),
-    path('post/ajax/friend', postFriend, name="post_friend"),
+    path("", FriendView.as_view(), name="friend_cbv"),
+    # path('', indexView),
+    # path('get/ajax/validate/nickname', checkNickName, name="validate_nickname"),
+    # path('post/ajax/friend', postFriend, name="post_friend"),
     # ...
 ]
